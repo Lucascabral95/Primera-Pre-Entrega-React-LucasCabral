95ddbd67804cbffe4a48bc689646b139c802f1c4
Lucas Cabral <lucassimple@hotmail.com>
2023-03-10 03:29:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lucas Cabral
         </a>
       </header>
     </div>
