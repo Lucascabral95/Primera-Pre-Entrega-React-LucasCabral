83c9bea463b8a763020849d0a39b295054749b6b
Lucas Cabral <lucassimple@hotmail.com>
2023-03-10 04:53:35 -0300

    Primera Pre Entrega de React.js de Lucas Cabral

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.scss'
+import { NavBar } from "./components/Navbar/Navbar"
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola!
-        </a>
-      </header>
+    <div>
+
+      <NavBar />
+
+      <ItemListContainer greeting="¡Bienvenidos, fans de Pokemon!" texto={"Pikachu esta muy feliz de recibir tu visita a su futura Tienda Oficial. 😊"} />
+
     </div>
   );
 }
